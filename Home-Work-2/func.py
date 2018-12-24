d0ee2f8e3717c5e447344f92fbcf1f5dfede1a56
saeedade <33999109+saeedade@users.noreply.github.com>
2018-12-24 21:14:19 +0100

    Assignment 2

---
+++ Home-Work-2/func.py
@@ -0,0 +1,21 @@
+#function declarations
+def song():
+    print("Lonely")
+
+def fileSize():
+    size="3.72mb"
+    print(size)
+
+def durationInSec():
+    s = 238
+    print(s)
+
+def bull():
+    var=True
+    print(var)
+
+#function call
+song()
+fileSize()
+durationInSec()
+bull()
